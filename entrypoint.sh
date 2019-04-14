e8f88608207e7cbace447427d84a0a5c1520870d
Raik Bieniek <raik.bieniek@gmail.com>
2019-04-14 09:42:50 +0200

    Make it possible to read tag name from a file.
    Make the message optional again.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 function validate_parameters() {
-  if [ -z "$TAG" ]; then
-    echo "ERROR: Tag name must be passed."
+
+  if [ [ ! -z $TAG_FILE ] && [ ! -f $TAG_FILE] ]; then
+    echo "ERROR: Tag file $TAG_FILE does not exist."
     usage
     exit
   fi
 
-  if [ -z "$MESSAGE" ]; then
-    echo "ERROR: A message must be passed."
+  if [ [ -z "$TAG" ] && [ -z $TAG_FILE ] ]; then
+    echo "ERROR: Tag name (-t or -x) must be passed."
     usage
     exit
   fi
-
 
   if [ -z "$GITHUB_SHA" ]; then
     echo "ERROR: The environment variable GITHUB_SHA musst be set."
@@ -39,8 +39,9 @@ function validate_parameters() {
 
 function usage() {
   echo "Usage: $0 [options]"
-  echo "    -t --tag         (required) The name for the Git tag to create for the release."
-  echo "    -m --message     (required) A text describing the content of this release.."
+  echo "    -t --tag         The name for the Git tag to create for the release."
+  echo "    -x --tag-file    A file that contains the name of the Git tag to create for the release."
+  echo "    -m --message     A text describing the content of this release."
   echo "    -p --prerelease  Mark this release as prerelease."
   echo "    -d --draft       Create a draft release instead of a normal one."
   echo "    -f --files       Files to attach to this release. Multiple files can be separated using a comma."
@@ -77,6 +78,16 @@ while :; do
         exit
       fi
       ;;
+    -x|--tag-file)
+      if [ "$2" ]; then
+        TAG_FILE=$2
+        shift
+      else
+        echo "ERROR: \"$1\" requires a non-empty option argument."
+        exit
+      fi
+      ;;
+
     -m|--message)
       if [ "$2" ]; then
         MESSAGE="--message=\"$2\""
@@ -112,6 +123,14 @@ done
 COMMIT="-t ${GITHUB_SHA}"
 
 validate_parameters
+
+if [ -z "$MESSAGE" ]; then
+  MESSAGE="--message=\"automatic release\""
+fi
+
+if [ ! -z $TAG_FILE ]; then
+  TAG=`cat $TAG_FILE`
+fi
 
 # Github REST API for creating releases: https://developer.github.com/v3/repos/releases/#create-a-release
 # Github CLI releases: https://hub.github.com/hub-release.1.html
